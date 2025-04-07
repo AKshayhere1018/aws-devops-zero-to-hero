b21c7842ee67b6f4af1499173e48065097d76a6c
Akshay Goud <akshaygoud1018@gmail.com>
2025-04-07 16:45:35 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+CONTAINER_NAME=pythonflask
+
+if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
+    docker rm -f $CONTAINER_NAME
+fi
+
 # Pull the Docker image from Docker Hub
 docker pull AKshayhere1018/pythonflask
 
