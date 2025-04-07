93605f4630e59fe1a0346c9b179d89b8878f360e
Akshay Goud <akshaygoud1018@gmail.com>
2025-04-07 16:37:01 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull AKshayhere1018/pythonflask
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5000:5000 AKshayhere1018/pythonflask
